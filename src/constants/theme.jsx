22f646f4a15326a4b0b4f60462217dd82d553491
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-17 23:19:35 +0200

    add theme for ThemeProvider

---
+++ src/constants/theme.jsx
@@ -0,0 +1,11 @@
+export const theme = Object.freeze({
+  colors: {
+    btnBackground: 'rgb(229, 9, 20)',
+    fontPrimary: 'rgb(255,255,255)',
+    fontSecondary: 'rgba(255,255,255,0.7)',
+    mainBackground: '#000',
+  },
+  border: {
+    onePx: `1px solid rgba(128,128,128,0.7)`,
+  },
+});
