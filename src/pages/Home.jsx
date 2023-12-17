8152d89163fcf6f08424796b259ea58a509bb0eb
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-17 23:20:26 +0200

    add Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+const Home = () => {
+  useEffect(() => {
+    // Query
+  }, []);
+
+  return (
+    <main>
+      <h1>Trending today</h1>
+      <ul>List</ul>
+    </main>
+  );
+};
+
+export default Home;
