70e29c8e4fef9a6d42ab60e068d322bdfef98844
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-17 23:20:03 +0200

    add ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from 'components/App';
+import { App } from 'components/App/App';
 import './reset.css';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './constants/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename='/goit-react-hw-05-movies'>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
