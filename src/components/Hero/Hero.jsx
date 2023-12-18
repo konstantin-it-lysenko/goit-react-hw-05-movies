443a451dbba8543d016d4885c06091eeaa91cb56
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:33:30 +0200

    add Hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import { Heading, Text } from './Hero.styled';
+
+const Hero = () => {
+  return (
+    <section style={{ padding: '80px 0 40px' }}>
+      <Heading>Trending today</Heading>
+      <Text>
+        Movies — scary, funny, dramatic, romantic — make us experience a whole
+        range of emotions. Many films — many impressions!
+      </Text>
+    </section>
+  );
+};
+
+export default Hero;
