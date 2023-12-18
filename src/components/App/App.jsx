90e7500ab8be4e97e69e163417672ebf340139bd
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:31:28 +0200

    upd App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,53 @@
 import Home from 'pages/Home';
 import MovieDetails from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import { AppWrapper } from './App.styled';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import {
+  Wrapper,
+  Header,
+  Logo,
+  NavList,
+  PageLink,
+  HeaderWrapper,
+} from './App.styled';
 
 export const App = () => {
   return (
-    <AppWrapper>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/movies">Movies</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-      </Routes>
-    </AppWrapper>
+    <>
+      <Header>
+        <Wrapper>
+          <HeaderWrapper>
+            <div>
+              <Logo>
+                <Link to={'/'}>LOGOFLIX</Link>
+              </Logo>
+            </div>
+            <nav>
+              <NavList>
+                <li>
+                  <PageLink to="/">HOME</PageLink>
+                </li>
+                <li>
+                  <PageLink to="/movies">MOVIES</PageLink>
+                </li>
+              </NavList>
+            </nav>
+          </HeaderWrapper>
+        </Wrapper>
+      </Header>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          {/* <Route path="/movies/:movieId/cast" element={<div>Cast</div>} /> */}
+          <Route
+            path="/movies/:movieId/reviews "
+            element={<div>Reviews</div>}
+          />
+        </Routes>
+      </Wrapper>
+    </>
   );
 };
