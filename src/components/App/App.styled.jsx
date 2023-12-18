39e50db194ab3bfca680442908fff7294d373cc9
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:30:01 +0200

    add App styles

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,14 +1,12 @@
 import styled from '@emotion/styled';
 import { theme } from 'constants';
+import { NavLink } from 'react-router-dom';
 
-export const AppWrapper = styled.div`
+export const Wrapper = styled.div`
   margin: 0 auto;
-  padding: 0 20px;
+  padding: 0 12px;
   max-width: 335px;
-  min-height: 100dvh;
-
   color: ${theme.colors.fontPrimary};
-  /* background: ${theme.colors.mainBackground}; */
 
   @media (min-width: 375px) {
     max-width: 375px;
@@ -17,7 +15,42 @@ export const AppWrapper = styled.div`
     max-width: 768px;
     padding: 0 32px;
   }
-  @media (min-width: 1440px) {
-    max-width: 1140px;
+  @media (min-width: 1024px) {
+    max-width: 100%;
+    padding: 0 48px;
+  }
+`;
+
+export const Header = styled.header`
+  background: ${theme.colors.background};
+  padding: 8px 0;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.span`
+  letter-spacing: -0.5px;
+  font-size: 1.7rem;
+  font-weight: 500;
+  color: ${theme.colors.logo};
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 20px;
+`;
+
+export const PageLink = styled(NavLink)`
+  padding: 4px 16px;
+  font-size: 0.7rem;
+  color: ${theme.colors.fontPrimary};
+  border-radius: 3px;
+  background: ${theme.colors.btnBackground};
+  &:hover {
+    background: ${theme.colors.active};
   }
 `;
