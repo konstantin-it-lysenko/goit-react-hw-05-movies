4db2843950e70817e31f88653e4ef8a5613746ae
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:30:37 +0200

    upd theme

--- src/constants/theme.jsx
+++ src/constants/theme.jsx
@@ -1,9 +1,11 @@
 export const theme = Object.freeze({
   colors: {
-    btnBackground: 'rgb(229, 9, 20)',
+    btnBackground: '#e50914',
+    logo: '#e50914',
+    active: '#ff131e',
     fontPrimary: 'rgb(255,255,255)',
     fontSecondary: 'rgba(255,255,255,0.7)',
-    mainBackground: '#000',
+    background: '#000',
   },
   border: {
     onePx: `1px solid rgba(128,128,128,0.7)`,
