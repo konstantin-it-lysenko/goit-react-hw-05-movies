448ad174e96c2318f081fbe1d7dba0709b710bdf
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:30:08 +0200

    add Hero styled

---
+++ src/components/Hero/Hero.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Heading = styled.h1`
+  margin-bottom: 16px;
+  font-weight: 900;
+  font-size: 2.4rem;
+`;
+
+export const Text = styled.p`
+  max-width: 360px;
+  word-spacing: 1px;
+  line-height: 1.2rem;
+  font-weight: 350;
+`;
