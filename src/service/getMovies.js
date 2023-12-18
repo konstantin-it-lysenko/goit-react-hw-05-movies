5e5c540e079e490d596a607481b26eedd776beb2
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:29:32 +0200

    fix axios params

--- src/service/getMovies.js
+++ src/service/getMovies.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 axios.defaults.params = {
-    API_KEY: '3405a2990e159ff0db5f2b7b81775051',
+    api_key: '3405a2990e159ff0db5f2b7b81775051',
 }
 
-export const getNovies = async () => {
+export const getTranding = async () => {
+    const { data } = await axios.get('trending/movie/week')
 
+    return data.results
+}
+
+export const getMovieById = async (id) => {
+    const { data } = await axios.get(`movie/${id}`)
+
+    return data
 }
