b949062cabf56ce776ca6e301bdc933150c0584a
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-18 22:32:37 +0200

    add logic and request for Movie

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,30 @@
+import Movie from 'components/Movie/Movie';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieById } from 'service/getMovies';
+
 const MovieDetails = () => {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    if (!movieId) return;
+    getSelectedMovie(movieId);
+  }, [movieId]);
+
+  const getSelectedMovie = async id => {
+    try {
+      const response = await getMovieById(id);
+      console.log('One movie', response);
+      setMovie(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <main>
+      {movie && <Movie movie={movie} />}
       <p>Additional information</p>
       <ul>
         <li>Cast</li>
