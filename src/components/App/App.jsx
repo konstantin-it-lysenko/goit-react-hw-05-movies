f5c4c8d8c1b525edddac4f9e0c2b7669371d9b4e
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-20 01:29:24 +0200

    remove header to SharedLayout component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,53 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
 import Home from 'pages/Home';
-import MovieDetails from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
-import { Link, Route, Routes } from 'react-router-dom';
-
-import {
-  Wrapper,
-  Header,
-  Logo,
-  NavList,
-  PageLink,
-  HeaderWrapper,
-} from './App.styled';
+import MovieDetails from 'pages/MovieDetails';
+import Cast from 'components/Cast/Cast';
+import Reviews from 'components/Reviews/Reviews';
 
 export const App = () => {
   return (
-    <>
-      <Header>
-        <Wrapper>
-          <HeaderWrapper>
-            <div>
-              <Logo>
-                <Link to={'/'}>LOGOFLIX</Link>
-              </Logo>
-            </div>
-            <nav>
-              <NavList>
-                <li>
-                  <PageLink to="/">HOME</PageLink>
-                </li>
-                <li>
-                  <PageLink to="/movies">MOVIES</PageLink>
-                </li>
-              </NavList>
-            </nav>
-          </HeaderWrapper>
-        </Wrapper>
-      </Header>
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-          {/* <Route path="/movies/:movieId/cast" element={<div>Cast</div>} /> */}
-          <Route
-            path="/movies/:movieId/reviews "
-            element={<div>Reviews</div>}
-          />
-        </Routes>
-      </Wrapper>
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="credits" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
