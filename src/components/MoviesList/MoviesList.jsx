b56c37116bd32152365a35caa81677586ddb24c8
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-12-20 01:31:01 +0200

    add section wrapper

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -3,25 +3,27 @@ import { List, Item, MovieLink, MovieTitle } from './MoviesList.styled';
 
 const MoviesList = ({ movies }) => {
   return (
-    <List>
-      {movies.map(({ id, original_title, poster_path }) => {
-        return (
-          <Item key={id}>
-            <MovieLink to={`/movies/${id}`}>
-              <img
-                src={
-                  poster_path
-                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                    : defaultPoster
-                }
-                alt={original_title}
-              />
-              <MovieTitle>{original_title}</MovieTitle>
-            </MovieLink>
-          </Item>
-        );
-      })}
-    </List>
+    <section style={{ padding: '40px 0' }}>
+      <List>
+        {movies.map(({ id, original_title, poster_path }) => {
+          return (
+            <Item key={id}>
+              <MovieLink to={`/movies/${id}`}>
+                <img
+                  src={
+                    poster_path
+                      ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                      : defaultPoster
+                  }
+                  alt={original_title}
+                />
+                <MovieTitle>{original_title}</MovieTitle>
+              </MovieLink>
+            </Item>
+          );
+        })}
+      </List>
+    </section>
   );
 };
 
